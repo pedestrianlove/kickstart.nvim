f0f07b2fb72a8439b714dde19b38fbdc2bc66213
J.S.LI <j.s.li@gapp.nthu.edu.tw>
2023-11-13 16:07:42 +0800

    Added telescope-file-browser.

---
+++ lua/custom/plugins/telescope-file-browser.lua
@@ -0,0 +1,13 @@
+return {
+    "nvim-telescope/telescope-file-browser.nvim",
+    dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+    config = function ()
+        require("telescope").load_extension "file_browser"
+        vim.api.nvim_set_keymap(
+            "n",
+            "<space>ff",
+            ":Telescope file_browser<CR>",
+            { noremap = true }
+        )
+    end
+}
