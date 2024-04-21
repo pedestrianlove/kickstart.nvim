e6940092baed2a659f11d9f8f40517116f7ce2bf
J.S.LI <j.s.li@gapp.nthu.edu.tw>
2024-04-21 13:07:04 +0800

    Add zenmode plugin and optimize the loading configuration.

--- lua/custom/plugins/markdown.lua
+++ lua/custom/plugins/markdown.lua
@@ -1,6 +1,9 @@
 return {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    ft = { "markdown" },
-    build = function() vim.fn["mkdp#util#install"]() end,
+  'iamcco/markdown-preview.nvim',
+  lazy = true,
+  cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+  ft = { 'markdown' },
+  build = function()
+    vim.fn['mkdp#util#install']()
+  end,
 }
